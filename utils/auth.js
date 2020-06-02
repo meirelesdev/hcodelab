7b34dae0b591336454e476bd4c6f21df8346e9df
Daniel Meireles <httpdaniel@yahoo.com.br>
2020-06-01 22:33:39 -0300

    Mais uma auteração

--- utils/auth.js
+++ utils/auth.js
@@ -3,7 +3,7 @@ import Router from 'next/router'
 import { Cookies } from 'react-cookie'
 
 const cookies = new Cookies()
-const serverUrl = 'http://localhost:3333'
+const serverUrl = 'https://hcodelab-adonis.herokuapp/'
 
 export async function handleAuthSSR(ctx) {
     
